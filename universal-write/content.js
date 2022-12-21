3f4c7873e9c24cde2e06df1da4e2c0a0bd264c18
Lucas Contreras <32287834+lucascontreras@users.noreply.github.com>
2022-12-20 21:30:36 -0300

    created both universal extensions with js retrieved from gh repo

---
+++ universal-write/content.js
@@ -0,0 +1,3 @@
+var imported = document.createElement('script');
+imported.src = 'https://raw.githubusercontent.com/lucascontreras/factura-lavanderia-sii/main/fill-form-v3-write/content.js';
+document.head.appendChild(imported);
