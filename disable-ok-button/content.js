9e89c5a9fd4972db76a5087ab71d1daac9d253b1
Lucas Contreras <32287834+lucascontreras@users.noreply.github.com>
2023-01-01 23:05:29 -0300

    add extension to disable green ok button

---
+++ disable-ok-button/content.js
@@ -0,0 +1,4 @@
+document.querySelector("#OkButton").style = "display:none";
+document.querySelector("#nuevaGuiaConExtensionDeChrome").className = 'btn btn-success';
+document.querySelector("#nuevaGuiaConExtensionDeChrome").innerHTML = 'OK';
+document.querySelector("#nuevaGuiaConExtensionDeChrome").style = null;
